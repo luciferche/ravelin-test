798752b45de09d3a71f23e51398109bd6366478c
luciferche <matovicluka@gmail.com>
2020-01-21 20:59:39 +0000

    update session based on event type

--- goapi/server.go
+++ goapi/server.go
@@ -37,17 +37,37 @@ var clientSessions map[string]*Data
 func homeHello(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Use POST request on /event API endpoint to send event data")
 }
-
 // updates session and returns flag if there was an error with params
 func updateSession(session *Data, ev event) bool {
-	return false
+	
+	switch ev.EventType {
+	case "copyAndPaste":
+		session.CopyAndPaste[ev.FormId] = true
+		// session.WebsiteUrl = ev.WebsiteUrl
+	case "screenResize":
+		session.ResizeFrom = ev.ResizeFrom
+		session.ResizeTo = ev.ResizeTo
+		// session.WebsiteUrl = ev.WebsiteUrl
+	case "timeTaken":
+		session.FormCompletionTime = ev.Time
+	default :
+		return true;
+	}
+	return false;
 }
-
-
 
 //constructor for mapping event from params to session data
 func newData(ev event) *Data {
 	d := new(Data)
+	d.ResizeTo = ev.ResizeTo
+	d.ResizeFrom = ev.ResizeFrom
+	d.WebsiteUrl = ev.WebsiteUrl 	//change to hash
+	d.CopyAndPaste = make(map[string]bool)
+	if ev.Pasted {
+		d.CopyAndPaste[ev.FormId] = true
+	}
+	d.FormCompletionTime = ev.Time
+	d.SessionId = ev.SessionId
 	return d
 }
 
@@ -68,7 +88,7 @@ func createEvent (w http.ResponseWriter, r *http.Request) {
 		err := updateSession(session, newEvent)
 		if err {
 			w.WriteHeader(http.StatusBadRequest)
-			json.NewEncoder(w).Encode("Wrong params ")
+			json.NewEncoder(w).Encode("Wrong params")
 			return
 		}
 	} else {
@@ -78,8 +98,8 @@ func createEvent (w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusCreated)
 
 	json.NewEncoder(w).Encode("")
+	// json.NewEncoder(w).Encode(newEvent)
 }
-
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/api", homeHello)
